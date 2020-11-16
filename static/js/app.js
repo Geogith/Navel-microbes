e9445d564de67ff9fd76cb852fb8c7522c98c151
Shunaam <67766966 + Geogith@users.noreply.github.com>
2020-11-16 13:57:37 -0600

    modified code in plot.js

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+// Load data from robdunnlab.com
+d3.tsv("http://robdunnlab.com/projects/belly-button-biodiversity");
+
+// .then(function (microbes) {
+console.log(microbes);
+
+//     // log a list of names
+//     var names = tvData.map((data) => data.name);
+//     console.log("names", names);
+
+//     // Cast each hours value in tvData as a number using the unary + operator
+//     tvData.forEach(function (data) {
+//       data.hours = +data.hours;
+//       console.log("Name:", data.name);
+//       console.log("Hours:", data.hours);
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+//   d3.tsv(url, callback);
---
+++ static/js/plot.js
@@ -0,0 +1,39 @@
+var microbes = {
+  x: [
+    "OTU 1167",
+    "OTU 2859",
+    "OTU 482",
+    "OTU 2264",
+    "OTU 41",
+    "OTU 1189",
+    "OTU 352",
+    "OTU 189",
+    "OTU 2318",
+    "OTU 1977",
+  ],
+
+  y: [155, 125, 119, 75, 65, 50, 49, 47, 46, 45],
+
+  type: "bar",
+};
+
+var data = [microbes];
+
+var layout = {
+  title: "'Horizontal Bar' Chart",
+  font: {
+    family: "Raleway, sans-serif",
+  },
+  showlegend: false,
+  xaxis: {
+    tickangle: -45,
+  },
+  yaxis: {
+    zeroline: false,
+    gridwidth: 2,
+  },
+  bargap: 0.05,
+};
+
+// Plotly.newPlot('myDiv', data, layout);
+Plotly.newPlot("horizontalbar-plot", data, layout);
